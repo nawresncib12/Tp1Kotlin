e40411031209d06d6aa94ad8252de13af410e956
Taher Jerbi <jerbi.taherr@gmail.com>
2022-10-14 10:42:28 +0100

    feat: pick image on click

--- app/src/main/java/com/example/tp1/PickImageActivity.kt
+++ app/src/main/java/com/example/tp1/PickImageActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -11,10 +12,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 class PickImageActivity : AppCompatActivity() {
     lateinit var imageView: ImageView
+    lateinit var pickImageBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_image)
         imageView = findViewById(R.id.imgView)
+        pickImageBtn = findViewById(R.id.pickImageButton)
+
+        pickImageBtn.setOnClickListener { view ->
+            loadImage(view)
+        }
     }
     fun loadImage(view: View){
         val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI )
